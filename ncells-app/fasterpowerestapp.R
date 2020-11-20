9201d4db3ce24a7d9f26d40df0ca12f7fc0406da
JoeFaski <faskij@mail.gvsu.edu>
2020-11-20 16:04:22 +0000

    updated 11/20/2020

--- ncells-app/fasterpowerestapp.R
+++ ncells-app/fasterpowerestapp.R
@@ -1,6 +1,9 @@
 
 
 library(shiny)
+library(shinyjs)
+library(shinybusy)
+library(shinybusy)
 library(quantmod)
 library('devtools')
 library('ncells')
@@ -12,16 +15,8 @@ data <- readRDS("data/plot_data.rds")
 # Define UI ----
 ui <- fluidPage(
   titlePanel(h1("Power Calculator for scRNA-seq Experiment")),
- 
-  mainPanel( 
-    plotOutput("plot"),
-    h2("Power Estimate"),
-    textOutput("n_cells"),
-    textOutput("ncells")
-    #tableOutput("ncells")
-   
-  ),
   
+  add_busy_spinner(spin = "fading-circle"),
   
   sidebarPanel(
     
@@ -44,79 +39,96 @@ ui <- fluidPage(
                   "1.5" = 1.5)),
   ),
   
-  sidebarPanel(
+  mainPanel( 
+    plotOutput("plot"),
     
-    fluidRow(
-      h3("Parameter Input for Power Estimate"),
-column(5, 
-       
-      
-      sliderInput("frac_m_genes",
-                  h5("Fraction of Marker Genes"),
-                  min = 0.1, max = 10, value = 1),
-      
-      sliderInput("n_cells",
-                  h5("Sample Size"),
-                  min = 50, max = 500, value = 100),
-      
-      sliderInput("percent_sub_pop",
-                  h5("Percentage of Sub Population"),
-                  min = 5, max = 50, value = 10),
-      
-      sliderInput("fold_ch",
-                  h5("Fold Change of Sub Pop"),
-                  min = 1, max = 10, value = 2, step = 0),
-      
-      numericInput("drop_rate",  h5("Drop out Rate"),
-                  min = 0.6, max = 0.999, value = 0.6)
-),
-
-column(7,
-      
-      
-      numericInput("unique_gene_count",
-                  h5("Unique Genes"),
-                  min = 5000, max = 25000 ,value = 17000),
-      
-      sliderInput("mean_expression",
-                  h5("Mean Gene Expression"),
-                  min = 0.5, max = 10, value = 1.5),
-      
-      sliderInput("standard_deviation",
-                  h5("Standard Deviation of Mean Expression"),
-                  min = 0.5, max = 10, value = 1),
-      
-      sliderInput("alpha",  h5("Alpha"),
-                  min = 0.01, max = 0.99, value = 0.05),
-      
-      
-      h4("This may take a couple of minutes to run"),
-      
-      actionButton("go", "Go")
-)
+    #tableOutput("ncells")
+    
+  ),
+  
+  sidebarPanel(h2("Power Estimate"),
+               textOutput("n_cells"),
+               textOutput("ncells"),
+               br(),
+               paste("__________________________"),
+               br(),
+               h4("(This may take a couple of minutes to run)"),
+               actionButton("go", "Go"),
+               add_busy_spinner(spin = "fading-circle")
+  ),
+  
+  
+  fluidRow(
+    h3("Parameter Input for Power Estimate"),
+    column(2, 
+           
+           
+           sliderInput("frac_m_genes",
+                       h5("Fraction of Marker Genes"),
+                       min = 0.1, max = 10, value = 1),
+           
+           sliderInput("n_cells",
+                       h5("Sample Size"),
+                       min = 50, max = 500, value = 100),
+           
+           sliderInput("percent_sub_pop",
+                       h5("Percentage of Sub Population"),
+                       min = 5, max = 50, value = 10) 
+    ),
+    
+    column(2,
+           
+           sliderInput("fold_ch",
+                       h5("Fold Change of Sub Pop"),
+                       min = 1, max = 10, value = 2, step = 0),
+           
+           numericInput("drop_rate",  h5("Drop out Rate"),
+                        min = 0.6, max = 0.999, value = 0.6),
+           
+           numericInput("unique_gene_count",
+                        h5("Unique Genes"),
+                        min = 5000, max = 25000 ,value = 17000)
+    ),
+    
+    column(3,
+           
+           sliderInput("mean_expression",
+                       h5("Mean Gene Expression"),
+                       min = 0.5, max = 10, value = 1.5),
+           
+           sliderInput("standard_deviation",
+                       h5("Standard Deviation of Mean Expression"),
+                       min = 0.5, max = 10, value = 1),
+           
+           sliderInput("alpha",  h5("Alpha"),
+                       min = 0.01, max = 0.99, value = 0.05) 
+    ),
+    
+   
     )
-    
-    
+  
   )
-)
 
 # Define server logic ----
 server <- function(input, output) {
 
   
   
-  size_reaction <- eventReactive( input$go, paste("You have selected a sample size of", input$n_cells) )
+  size_reaction <- eventReactive( input$go,  
+    
+    paste("You have selected a sample size of", input$n_cells)
+    )
     
   output$n_cells <- renderText({
     size_reaction()
   })
   
+
+  power_reaction <- eventReactive(input$go,
+    powerEst <- ncells(m1 = input$frac_m_genes, pi1=input$percent_sub_pop, foldchange = input$fold_ch, dropout = input$drop_rate, 
+    p = input$unique_gene_count, n = input$n_cells, mu = input$mean_expression, sigma = input$standard_deviation, type1 = input$alpha, dfactor= T, B = 100)
   
-  power_reaction <- eventReactive(input$go, 
-            
-    powerEst <- ncells(m1 = input$frac_m_genes, pi1=input$percent_sub_pop, foldchange = input$fold_ch, dropout = input$drop_rate, p = input$unique_gene_count, 
-                                                     n = input$n_cells, mu = input$mean_expression, sigma = input$standard_deviation, type1 = input$alpha, dfactor= T, B = 100),
-                                  
+    
                                   )
  
   output$ncells <- renderText({
@@ -125,6 +137,7 @@ server <- function(input, output) {
   
   })
   
+ 
   output$plot <- renderPlot({
     
     data%>%
